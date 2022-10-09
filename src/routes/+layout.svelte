b83d7cc24181040110988460723aea6313039655
Asendia Mayco <asendia@icloud.com>
2022-10-09 23:42:32 +0700

    Add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,3 +5,17 @@
 <div class="p-4">
 	<slot />
 </div>
+
+<footer class="p-3 text-center">
+	2022
+	<br />
+	sejiwo - source code:
+	<a
+		class="text-blue-500 underline"
+		target="_blank"
+		rel="noopener noreferrer"
+		href="https://github.com/asendia/names-and-faces"
+	>
+		github
+	</a>
+</footer>
