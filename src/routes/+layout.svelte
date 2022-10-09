9fbfa54edb3a6afe5a8d50afbbd9e4ef3e6064af
Asendia Mayco <asendia@icloud.com>
2022-10-10 01:40:39 +0700

    Adjust footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,20 +2,22 @@
 	import '../app.css';
 </script>
 
-<div class="p-4">
+<div class="p-4 min-h-[90vh]">
 	<slot />
 </div>
 
-<footer class="p-3 text-center">
+<footer class="p-3 h-[10vh] flex flex-col items-center justify-center">
 	2022
 	<br />
-	sejiwo - source code:
-	<a
-		class="text-blue-500 underline"
-		target="_blank"
-		rel="noopener noreferrer"
-		href="https://github.com/asendia/names-and-faces"
-	>
-		github
-	</a>
+	<div class="text-gray-500 text-sm">
+		sejiwo -
+		<a
+			class="text-blue-500 underline"
+			target="_blank"
+			rel="noopener noreferrer"
+			href="https://github.com/asendia/names-and-faces"
+		>
+			source code
+		</a>
+	</div>
 </footer>
